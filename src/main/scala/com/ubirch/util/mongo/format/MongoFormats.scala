e7752b5663d4b08798534d10cec70988eb97cd84
Lotta Rüger <lotta.rueger@ubirch.com>
2022-09-14 11:06:51 +0200

    fixing the BSON handler for Big Decimal and UUIDs

--- src/main/scala/com/ubirch/util/mongo/format/MongoFormats.scala
+++ src/main/scala/com/ubirch/util/mongo/format/MongoFormats.scala
@@ -5,15 +5,17 @@ import reactivemongo.api.bson.{
   BSONBinary,
   BSONDateTime,
   BSONDocument,
+  BSONDocumentHandler,
   BSONDocumentReader,
   BSONDocumentWriter,
-  BSONDouble,
   BSONHandler,
   BSONValue,
   Subtype
 }
 
-import scala.util.{ Failure, Success, Try }
+import java.nio.ByteBuffer
+import java.util.UUID
+import scala.util.Try
 
 /**
   * author: cvandrei
@@ -21,35 +23,69 @@ import scala.util.{ Failure, Success, Try }
   */
 trait MongoFormats {
 
-  implicit protected object BSONDateTimeHandler extends BSONHandler[DateTime] {
+  implicit protected object uuidHandler extends BSONHandler[UUID] {
+    override def writeTry(id: UUID): Try[BSONValue] =
+      Try {
+        val bb = ByteBuffer.wrap(new Array[Byte](16))
+        bb.putLong(id.getMostSignificantBits)
+        bb.putLong(id.getLeastSignificantBits)
+        BSONBinary(bb.array(), Subtype.UuidSubtype)
+      }
 
-    override def readTry(bson: BSONValue): Try[DateTime] = bson match {
-      case BSONDateTime(long) => Success(new DateTime(long).withZone(DateTimeZone.UTC))
-      case _                  => Failure(new IllegalArgumentException())
+    override def readTry(bson: BSONValue): Try[UUID] = {
+      bson.asTry[BSONBinary].map { binary =>
+        val byteBuffer = ByteBuffer.wrap(binary.byteArray)
+        val high = byteBuffer.getLong()
+        val low = byteBuffer.getLong()
+        new UUID(high, low)
+      }
     }
-
-    override def writeTry(t: DateTime): Try[BSONDateTime] = Try(BSONDateTime(t.withZone(DateTimeZone.UTC).getMillis))
   }
 
-  implicit object BigDecimalHandler extends BSONHandler[BigDecimal] {
-    def readTry(v: BSONValue): Try[BigDecimal] = v match {
-      case BSONDouble(double) => Success(BigDecimal(double))
-      case _                  => Failure(new IllegalArgumentException())
-    }
+  implicit protected object BSONDateTimeHandler extends BSONHandler[DateTime] {
 
-    def writeTry(bd: BigDecimal): Success[BSONDouble] = Success(BSONDouble(bd.toDouble))
+    override def readTry(bson: BSONValue): Try[DateTime] =
+      bson
+        .asTry[BSONDateTime]
+        .map { bsonDateTime =>
+          new DateTime(bsonDateTime.value).withZone(DateTimeZone.UTC)
+        }
+
+    override def writeTry(dateTime: DateTime): Try[BSONDateTime] =
+      Try(BSONDateTime(dateTime.withZone(DateTimeZone.UTC).getMillis))
   }
 
   implicit object BigIntHandler extends BSONDocumentReader[BigInt] with BSONDocumentWriter[BigInt] {
 
-    def writeTry(bigInt: BigInt): Success[BSONDocument] = Success(BSONDocument(
+    def writeTry(bigInt: BigInt): Try[BSONDocument] = Try(BSONDocument(
       "signum" -> bigInt.signum,
       "value" -> BSONBinary(bigInt.toByteArray, Subtype.UserDefinedSubtype)))
 
-    def readDocument(doc: BSONDocument): Try[BigInt] = for {
-      sig <- doc.getAsTry[Int]("signum")
-      bin <- doc.getAsTry[BSONBinary]("value")
-    } yield BigInt(sig, bin.byteArray)
+    def readDocument(doc: BSONDocument): Try[BigInt] =
+      for {
+        sig <- doc.getAsTry[Int]("signum")
+        bin <- doc.getAsTry[BSONBinary]("value")
+      } yield BigInt(sig, bin.byteArray)
   }
 
+  implicit object BigDecimalHandler extends BSONDocumentHandler[BigDecimal] {
+
+    override def writeTry(bd: BigDecimal): Try[BSONDocument] = Try {
+      BSONDocument(
+        "scale" -> bd.scale,
+        "precision" -> bd.precision,
+        "value" -> BigInt(bd.underlying.unscaledValue())
+      )
+    }
+
+    override def readDocument(doc: BSONDocument): Try[BigDecimal] =
+      for {
+        value <- doc.getAsTry[BigInt]("value")
+        scale <- doc.getAsTry[Int]("scale")
+        precision <- doc.getAsTry[Int]("precision")
+      } yield {
+        val mc = new java.math.MathContext(precision)
+        BigDecimal.apply(value, scale, mc)
+      }
+  }
 }
